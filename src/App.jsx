aa4fdf49127bd903bd949ed132011c8e279c6194
dvargasclicOH <dvargas@clicoh.com>
2025-06-09 14:12:11 -0500

    name container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { Container } from './Container';
+import { Container } from './Container.jsx';
 function App() {
   const [formData, setFormData] = useState({
     campo1: '',
